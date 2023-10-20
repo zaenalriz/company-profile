9bb04d3a3ba207b8688db57e117d04f24bbe7cdb
Muhammad Zaenal <Muhammad_Zaenal77>
2023-10-20 08:44:03 +0700

    commit pertama

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <Navbar></Navbar>
+  <div class="container">
+    <Section1></Section1>
+    <Section2></Section2>
+  </div>
+</template>
+
+<script>
+import Navbar from '@/components/Home/Navbar.vue';
+import Section1 from '@/components/Home/Section1.vue';
+import Section2 from '@/components/Home/Section2.vue';
+export default {
+  components: {
+    Navbar,
+    Section1,
+    Section2
+  }
+}
+</script>
+<style scope></style>
