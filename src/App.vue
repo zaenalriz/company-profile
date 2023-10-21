5a1d4ea333fe908ea3c1438071b5db044bd2d229
Muhammad Zaenal <Muhammad_Zaenal77>
2023-10-21 17:17:30 +0700

    feat(router) : menambahkan router

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,15 @@
 <template>
   <Navbar></Navbar>
-  <div class="container">
-    <Section1></Section1>
-    <Section2></Section2>
-    <Portfolio></Portfolio>
-    <Blog></Blog>
-  </div>
+  <router-view></router-view>
   <Footer></Footer>
 </template>
 
 <script>
 import Navbar from '@/components/Layouts/Navbar.vue';
-import Section1 from '@/components/Home/Section1.vue';
-import Section2 from '@/components/Home/Section2.vue';
-import Portfolio from '@/components/Home/Portfolio.vue';
-import Blog from '@/components/Home/Blog.vue';
 import Footer from '@/components/Layouts/Footer.vue';
 export default {
   components: {
     Navbar,
-    Section1,
-    Section2,
-    Portfolio,
-    Blog,
     Footer
   }
 }
--- src/main.js
+++ src/main.js
@@ -1,6 +1,16 @@
 import "./assets/style.css";
+import { createRouter, createWebHistory } from "vue-router";
+
+import Home from "@/pages/Home.vue";
+const routes = [{ path: "/", component: Home }];
+
+const router = createRouter({
+  // 4. Provide the history implementation to use. We are using the hash history for simplicity here.
+  history: createWebHistory(),
+  routes, // short for `routes: routes`
+});
 
 import { createApp } from "vue";
 import App from "./App.vue";
 
-createApp(App).mount("#app");
+createApp(App).use(router).mount("#app");
